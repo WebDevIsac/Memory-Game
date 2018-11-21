f24037346b155a8b7c8a6998dd520cc1d8591f0b
WebDevIsac <isac.code@gmail.com>
2018-11-21 17:15:32 +0100

    Added function to shuffle tiles array

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+
+
+// Define variables
+const container = document.querySelector('.container');
+const gameContainer = document.querySelector('.game-container');
+
+let tiles = document.querySelectorAll('.game-container > div');
+let tilesArray = Array.from(tiles);
+
+
+// Function to shuffle array
+function shuffle (array) {
+    let i = 0;
+    let j = 0;
+    let temp = null
+  
+    for (i = array.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+    return array;
+}
+
+console.log(shuffle(tilesArray));
+
