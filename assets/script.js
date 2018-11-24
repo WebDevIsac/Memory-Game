2ff7226fbe1ff68a325dc2cb5dd42857e9319981
WebDevIsac <isac.code@gmail.com>
2018-11-24 20:14:36 +0100

    Added function to check if hard mode is actviated or not

--- assets/script.js
+++ assets/script.js
@@ -5,32 +5,57 @@ const container = document.querySelector('.container');
 const gameContainer = document.querySelector('.game-container');
 const tiles = document.querySelectorAll('.game-container > div');
 const hardModeTiles = document.querySelectorAll('.hard-mode');
+const easyBtn = document.querySelector('.easy-mode');
+const mediumBtn = document.querySelector('.medium-mode');
+const hardBtn = document.querySelector('.hard-mode');
 
-
+// Check if game is set to Hard mode
+function checkMode () {
+  if (gameContainer.classList.contains('game-container-hard')) {
+    for (let i = 0; i < hardModeTiles.length; i++) {
+      hardModeTiles[i].classList.add('hide');
+    }
+    gameContainer.classList.remove('game-container-hard');
+  }
+}
 
 // Easy mode
 function easyMode () {
-  for (let i = tiles.length; i--;) {
+  checkMode();
+  for (let i = 0; i < tiles.length; i++) {
     gameContainer.appendChild(tiles[Math.random() * i | 0]);
   }
 }
 
-// Medium mode
 function mediumMode () {
-  let temp = gameContainer.cloneNode(true); 
-  // shuffle the cloned list (better performance)
-  for (let i = temp.children.length + 1; i--; ) {
-    temp.appendChild(temp.children[Math.random() * i | 0]);
+  checkMode();
+  for (let i = 0; i < tiles.length; i++) {
+    gameContainer.appendChild(tiles[Math.random() * i | 0]);
   }
-  gameContainer.parentNode.replaceChild(temp, gameContainer);
 }
 
-// Hard mode
-function hardMode () {
-  for (let i = 0; i < hardModeTiles.length; i++) {
-    hardModeTiles[i].classList.remove('hide');
-    gameContainer.classList.add('game-container-hard-mode');
-  }
-  mediumMode();
+// // Medium mode
+// function mediumMode () {
+  //   let temp = gameContainer.cloneNode(true); 
+  //   // shuffle the cloned list (better performance)
+  //   for (let i = temp.children.length; i--;) {
+    //     temp.appendChild(temp.children[Math.random() * i | 0]);
+    //   }
+    //   gameContainer.parentNode.replaceChild(temp, gameContainer);
+    // }
+    
+    
+    // Hard mode
+    function hardMode () {
+      for (let i = 0; i < hardModeTiles.length; i++) {
+        hardModeTiles[i].classList.remove('hide');
+      }
+      for (let i = 0; i < tiles.length; i++) {
+        gameContainer.appendChild(tiles[Math.random() * i | 0]);
+      }
+  gameContainer.classList.add('game-container-hard');
 }
 
+easyBtn.addEventListener('click', easyMode);
+mediumBtn.addEventListener('click', mediumMode);
+hardBtn.addEventListener('click', hardMode);
