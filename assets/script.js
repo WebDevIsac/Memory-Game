6381f83e3427e543d2b1001ef7519bb6f3175c4d
WebDevIsac <isac.code@gmail.com>
2018-11-27 11:54:00 +0100

    Updated html and css

--- assets/script.js
+++ assets/script.js
@@ -5,63 +5,131 @@ const container = document.querySelector('.container');
 const gameContainer = document.querySelector('.game-container');
 const cards = document.querySelectorAll('.card');
 const hardModeCards = document.querySelectorAll('.hard-card');
+const difficulty = document.querySelector('.difficulty');
+const start = document.querySelector('.start');
+const startBtn = start.querySelector(':first-child');
+const difficultyBtn = start.querySelector(':last-child');
 const easyBtn = document.querySelector('.easy-mode');
 const mediumBtn = document.querySelector('.medium-mode');
 const hardBtn = document.querySelector('.hard-mode');
-
+const backMenu = document.querySelector('.back-menu');
+const backMenuBtn = backMenu.querySelector('h1')
 const frontCards = document.querySelectorAll('.front-card');
 const backCards = document.querySelectorAll('.back-card');
 
-function hideCards () {
-  for (let i = 0; i < frontCards.length; i++) {
-    frontCards[i].classList.add('hide');
+let mode = 'easy';
+
+startBtn.addEventListener('click', startGame);
+difficultyBtn.addEventListener('click', chooseDifficulty);
+backMenuBtn.addEventListener('click', backToMenu);
+
+function startGame () {
+  startBtn.parentElement.classList.add('hide');
+  for (let i = 0; i < cards.length - 4; i++) {
+    cards[i].classList.remove('hide');
   }
-}
-
-hideCards();
-
-
-// Check if game is set to Hard mode
-function checkMode () {
-  if (gameContainer.classList.contains('game-container-hard')) {
-    for (let i = 0; i < hardModeCards.length; i++) {
-      hardModeCards[i].classList.add('hide');
-    }
+  if (mode === 'easy') {
+    easyMode();
     gameContainer.classList.remove('game-container-hard');
+  } 
+  else if (mode === 'medium') {
+    mediumMode();
+    gameContainer.classList.remove('game-container-hard');
+  } 
+  else if (mode === 'hard') {
+    hardMode();
+    for (let i = 0; i < hardModeCards.length; i++) {
+      hardModeCards[i].classList.remove('hide');
+    }
+    gameContainer.classList.add('game-container-hard');
+  }
+  backMenu.classList.remove('hide');
+}
+
+function backToStart () {
+  startBtn.parentElement.classList.remove('hide');
+  difficulty.classList.add('hide');
+  difficultyBtn.innerHTML = mode;
+}
+
+function chooseDifficulty () {
+  difficulty.classList.remove('hide');
+  startBtn.parentElement.classList.add('hide');
+}
+
+function backToMenu () {
+  resetCards();
+  backToStart();
+  backMenu.classList.add('hide');
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].classList.add('hide');
   }
 }
+
+function resetCards () {
+  for (let i = 0; i < cards.length; i++) {
+    gameContainer.appendChild(cards[i]);
+  }
+}
+
 
 // Easy mode
 function easyMode () {
-  checkMode();
-  for (let i = 0; i < 17; i++) {
+  resetCards();
+  for (let i = 0; i < 16; i++) {
     let random = Math.floor(Math.random() * i);
     gameContainer.appendChild(cards[random]);
   }
 }
 
+// Medium mode
 function mediumMode () {
-  checkMode();
-  for (let i = 0; i < 17; i++) {
-    let random = Math.floor((Math.random() * 17));
+  resetCards();
+  for (let i = 0; i < 16; i++) {
+    let random = Math.floor((Math.random() * 16));
     gameContainer.appendChild(cards[random]);
   }
 }
 
 // Hard mode
 function hardMode () {
+  resetCards();
   for (let i = 0; i < hardModeCards.length; i++) {
     hardModeCards[i].classList.remove('hide');
   }
   for (let i = 0; i < 21; i++) {
-    let random = Math.floor((Math.random() * 21));
+    let random = Math.floor((Math.random() * 20));
     gameContainer.appendChild(cards[random]);
   }
   gameContainer.classList.add('game-container-hard');
 }
 
-easyBtn.addEventListener('click', easyMode);
-mediumBtn.addEventListener('click', mediumMode);
-hardBtn.addEventListener('click', hardMode);
+// Eventlisteners for game difficulty on click 
+easyBtn.addEventListener('click', () => {
+  mode = 'easy';
+  backToStart();
+});
+mediumBtn.addEventListener('click', () => {
+  mode = 'medium';
+  backToStart();
+});
+hardBtn.addEventListener('click', () => {
+  mode = 'hard';
+  backToStart();
+});
+
+// Flip cards
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener('click', () => {
+    cards[i].classList.add('flip');
+    setTimeout(() => {
+      frontCards[i].classList.add('hide');
+      backCards[i].classList.remove('hide'); 
+    }, 600);
+    setTimeout(() => {
+      cards[i].classList.remove('flip');
+    }, 1000);
+  });
+}
 
 
